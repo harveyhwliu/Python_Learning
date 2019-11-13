0316c8be86a0961aefefb130154b7ea6ef1154b2
huawei1991 <2580205897@qq.com>
2019-11-13 20:24:59 +0800

    first commit for add 004 lessons

---
+++ Learning/Python 进阶/004map_reduce_filter的使用/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#-*-coding:utf-8-*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+def test_map_demo1():
+    pass
+
+
+def main():
+    test_map_demo1()
+
+if __name__ == "__main__":
+    main()
+
