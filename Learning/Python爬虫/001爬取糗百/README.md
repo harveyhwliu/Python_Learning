eb869ddc4402c87e17893d42441b856d32365ed8
huawei1991 <2580205897@qq.com>
2019-11-21 22:12:06 +0800

    1

---
+++ Learning/Python爬虫/001爬取糗百/README.md
@@ -0,0 +1,8 @@
+## 1 requests 库介绍
+   1. 需要安装各种库，参考源码import 包名来安装  pip install * -t path 或者pycharm 通过配置解释器来安装包
+   2. TODO:访问https
+
+   ```python
+
+```
+
--- Learning/Python爬虫/001爬取糗百/main.py
+++ Learning/Python爬虫/001爬取糗百/main.py
@@ -1,14 +1,42 @@
 #!/usr/bin/python
 #coding:utf-8
 import traceback
-from pprint import pprint
+import os
+#导入reuqest网络库
+try:
+    import urllib.request as urllib_request     # for Python 3
+    from urllib.request import urlretrieve
+except ImportError:
+    import urllib2 as urllib_request            # for Python 2
+    from urllib import urlretrieve
+
+#导入beautifulsoup
+from bs4 import BeautifulSoup as bf
+
+def test():
+    html = urllib_request.urlopen("https://www.baidu.com")
+    #用beautifulSoup解析html
+    obj = bf(html.read(),'html.parser')
+    #打印标题
+    title = obj.head.title
+    print(title)
+    #使用find_all 获取所有的图片信息,只提取logo图片的信息
+    pic_info = obj.find_all('img',class_="index-logo-src")
+    #提取logo 图片的信息：
+    image_path = "./img/"
+    if not os.path.exists(image_path):
+        os.mkdir(image_path)
+    for _index,pic in enumerate(pic_info):
+        log_url="{0}{1}".format("https:",pic["src"])
+        #下载图片
+        urlretrieve(log_url,"{}logo{}.png".format(image_path,_index))
 
 def main():
     try:
-        pass
+        test()
     except Exception as e:
         errorMsg = '爬取失败，异常{},{}'.format(str(e),traceback.format_exc())
-        pprint(errorMsg)
+        print(errorMsg)
 
 if __name__ == "__main__":
     main()
